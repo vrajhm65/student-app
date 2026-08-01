c358859a3b4195681dee9d0b6e592e0be8ba6f4e
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-01 16:18:46 +0530

    auto-commit

--- smart-productivity-dashboard/server/server.js
+++ smart-productivity-dashboard/server/server.js
@@ -2,9 +2,9 @@ const express = require("express"); // importing express library
 const app = express(); // creating express app
 const PORT = 5000; // choosing port
 app.get("/", (req, res)=>{
-    res.send("Smart Productivity Dashboard is running")
+    res.send("Smart Productivity Dashboard Backend is running")
 });
 res.send()
 app.listen(PORT, () => {
-    console.log(`server is running on http://localhost:${port}`)
+    console.log(`server is running on http://localhost:${PORT}`)
 });
