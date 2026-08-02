fdca35768edd96d73d641ad292b6df48df08d33e
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-02 15:50:18 +0530

    auto-commit

--- smart-productivity-dashboard/server/server.js
+++ smart-productivity-dashboard/server/server.js
@@ -1,7 +1,13 @@
 const express = require("express"); // importing express library
+
+const taskRoutes = require("./routes/taskRoutes");
+
 const app = express(); // creating express app
-app.use(express.json()); // express needs to understand the json .so it acts as a translator
+
 const PORT = 5000; // choosing port
+
+app.use(express.json()); // express needs to understand the json .so it acts as a translator
+app.use("/api/tasks", taskRoutes);
 let tasks=[
     {
     id: 1,
@@ -13,8 +19,8 @@ app.get("/", (req, res)=>{
     res.send("Smart Productivity Dashboard Backend is running")
 });
 
-const taskRoutes = require("./routes/taskRoutes");
-app.use("/api/tasks", taskRoutes);
+
+
 
 app.post("/api/tasks", (req, res)=> {
     const newTask = req.body;
