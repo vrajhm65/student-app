7d6285375c886f8caf2779dfa58b535ef2e23278
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-02 15:45:08 +0530

    auto-commit

--- smart-productivity-dashboard/server/routes/taskRoutes.js
+++ smart-productivity-dashboard/server/routes/taskRoutes.js
@@ -3,5 +3,5 @@ const router=express.Router(); //  each feature gets own router i.e mini router
 
 const{ getTasks }= require("../controllers/taskController"); // imports get tasjs function from controller folder
 
-router.get("/",getTasks);
-module.exports=router;
+router.get("/",getTasks); // routes home path
+module.exports=router; //exports router to srver.js
