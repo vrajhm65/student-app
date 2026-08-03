1d11a06f81712018efcc58129ed71ca6e3aaef77
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-03 14:22:25 +0530

    auto-commit

--- smart-productivity-dashboard/server/routes/taskRoutes.js
+++ smart-productivity-dashboard/server/routes/taskRoutes.js
@@ -1,8 +1,10 @@
 const express=require("express"); // imports express
 const router=express.Router(); //  each feature gets own router i.e mini router
 
-const{ getTasks, getTaskById }= require("../controllers/taskController"); // imports get tasjs function from controller folder
+const{ getTasks, getTaskById, createTask }= require("../controllers/taskController"); // imports get tasjs function from controller folder
 
 router.get("/",getTasks); // routes home path
 router.get("/:id",getTaskById);
+router.post("/",createTask);
+
 module.exports=router; //exports router to srver.js
