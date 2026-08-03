a34a5f481533225cf1dad09dbcb832eab157d9b1
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-03 14:04:31 +0530

    auto-commit

--- smart-productivity-dashboard/server/controllers/taskController.js
+++ smart-productivity-dashboard/server/controllers/taskController.js
@@ -8,10 +8,11 @@ const getTasks=(req, res) =>{
 };
 const getTaskById =(req, res)=>{
     const taskId = Number(req.params.id); // rreq params id reffers to "5" if url is /api/tasks/id. notice its a string and convert into number
-    const task = tasks.find(task => task.id ===taskId);
+    const task = tasks.find(task => task.id ===taskId); // searches the array and returns first matching task
 
     if(!task){
-        return res.status(404).json({
+        return res.status(404).json   // 404 if no task exists meanas request task not found 
+        ({
             message: "Task not found"
         });
     }
