989211075ad355fc98f8e51291bbae6222ce9b46
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-03 13:59:51 +0530

    auto-commit

--- smart-productivity-dashboard/server/controllers/taskController.js
+++ smart-productivity-dashboard/server/controllers/taskController.js
@@ -6,8 +6,19 @@ completed: false
 const getTasks=(req, res) =>{
     res.json(tasks);
 };
+const getTaskById =(req, res)=>{
+    const taskId = Number(req.params.id); // 
+    const task = tasks.find(task => task.id ===taskId);
 
+    if(!task){
+        return res.status(404).json({
+            message: "Task not found"
+        });
+    }
+    res.json(task);
+}
 module.exports={
-    getTasks
+    getTasks,
+    getTaskById
 };
 
