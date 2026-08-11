1de5d1709796672b99b16bf27ebc4fa77e6128f4
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-11 19:04:09 +0530

    auto-commit

---
+++ smart-productivity-dashboard/client/src/pages/Tasks.jsx
@@ -0,0 +1,17 @@
+function Tasks() {
+  return (
+    <div className="simple-page">
+      <p className="section-label">TASKS</p>
+      <h1>All Tasks</h1>
+      <p>
+        Manage everything you need to accomplish.
+      </p>
+
+      <div className="page-placeholder">
+        Task management space
+      </div>
+    </div>
+  );
+}
+
+export default Tasks;
