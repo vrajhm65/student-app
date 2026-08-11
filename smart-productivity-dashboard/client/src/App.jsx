76a45921433984b84e7fae7e2f296dc7675655dc
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-11 18:59:49 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/App.jsx
+++ smart-productivity-dashboard/client/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Tasks from "./pages/Tasks";
+import Daily from "./pages/Daily";
+import Calendar from "./pages/Calendar";
+import Settings from "./pages/Settings";
 
 function App() {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/daily" element={<Daily />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/settings" element={<Settings />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
