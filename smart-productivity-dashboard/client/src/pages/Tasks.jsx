8b822112663cdddcf1ff76ca3ead87dfa713e637
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-14 15:24:20 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/pages/Tasks.jsx
+++ smart-productivity-dashboard/client/src/pages/Tasks.jsx
@@ -1,4 +1,23 @@
-<div className="add-task">
+import { useEffect, useState } from "react";
+function Tasks() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/api/tasks")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("tasks recived from backend", data);
+        setTasks(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching tasks:", error);
+      });
+  }, []);
+  return (
+    <div className="simple-page">
+      <p className="section-label">TASKS</p>
+      <h1>All Tasks</h1>
+      <p>Manage everything you need to accomplish.</p>
+        <div className="add-task">
     <input
         type="text"
         placeholder="Enter a new task"
@@ -35,27 +54,6 @@
         Add Task
     </button>
 </div>
-
-import { useEffect, useState } from "react";
-function Tasks() {
-  const [tasks, setTasks] = useState([]);
-  useEffect(() => {
-    fetch("http://localhost:5000/api/tasks")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("tasks recived from backend", data);
-        setTasks(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching tasks:", error);
-      });
-  }, []);
-  return (
-    <div className="simple-page">
-      <p className="section-label">TASKS</p>
-      <h1>All Tasks</h1>
-      <p>Manage everything you need to accomplish.</p>
-
       <div className="task-list">
         {tasks.map((task) => (
           <div className="task-card" key={task.id}>
