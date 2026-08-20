e4483edd6d069a7fda4146699439b04f3c44dee1
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-20 22:31:50 +0530

    auto-commit

---
+++ smart-productivity-dashboard/server/models/task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+
+        completed: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("Task", taskSchema);
