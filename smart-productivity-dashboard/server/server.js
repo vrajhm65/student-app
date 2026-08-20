ab535e27105358263d49275eed9ea0fe66dd9722
Vinayraj hiremath <vrajhm65@gmail.com>
2026-08-20 21:52:19 +0530

    auto-commit

--- smart-productivity-dashboard/server/server.js
+++ smart-productivity-dashboard/server/server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const express = require("express");  // importing express library
 const connectDB = require("./config/db");
