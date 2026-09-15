24fbd269508839a9f1dd07d264d6f212e3b3ccaa
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 11:26:52 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/components/focusTimer.jsx
+++ smart-productivity-dashboard/client/src/components/focusTimer.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function FocusTimer({ seconds, setSeconds }) {
+function FocusTimer({ seconds, setSeconds, setSessions }) {
   const [running, setRunning] = useState(false);
   const [loading, setLoading] = useState(true);
 
