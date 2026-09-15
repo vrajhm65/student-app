3161283061106d9f47404dfa73db86479614dcba
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 11:48:20 +0530

    auto-commit

--- smart-productivity-dashboard/server/controllers/authController.js
+++ smart-productivity-dashboard/server/controllers/authController.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
-const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const registerUser = async (req, res) => {
     try {
@@ -44,6 +45,66 @@ const registerUser = async (req, res) => {
     }
 };
 
+
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({
+                message: "Email and password are required"
+            });
+        }
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const passwordMatch = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if (!passwordMatch) {
+            return res.status(401).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id.toString()
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "7d"
+            }
+        );
+
+        res.json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id.toString(),
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        console.error("Login error:", error);
+
+        res.status(500).json({
+            message: "Failed to login"
+        });
+    }
+};
+
+
 module.exports = {
-    registerUser
+    registerUser,
+    loginUser
 };
