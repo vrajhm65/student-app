07477052e9af359786a3c35a228dda9c888fa40c
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 23:41:59 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/pages/login.jsx
+++ smart-productivity-dashboard/client/src/pages/login.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link} from "react-router-dom";
 
 function Login() {
   const navigate = useNavigate();
