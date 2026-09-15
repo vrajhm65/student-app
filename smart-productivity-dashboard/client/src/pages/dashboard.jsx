25a60d73e03fcd9da59d8ed6f86d4f19e54df18e
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 11:17:28 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/pages/dashboard.jsx
+++ smart-productivity-dashboard/client/src/pages/dashboard.jsx
@@ -13,6 +13,7 @@ function Dashboard() {
     const [tasks, setTasks] = useState([]);
     const [focusSeconds, setFocusSeconds] = useState(0);
     const [focusSessions, setFocusSessions] = useState([]);
+    const [currentStreak, setCurrentStreak] = useState(0);
 
     useEffect(() => {
         fetch("http://localhost:5000/api/tasks")
