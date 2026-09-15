9266e7ac59c4beb37beecee1343f29577d2e45d2
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 18:42:36 +0530

    auto-commit

--- smart-productivity-dashboard/server/models/plan.js
+++ smart-productivity-dashboard/server/models/plan.js
@@ -1,32 +1,33 @@
 const mongoose = require("mongoose");
 
 const planSchema = new mongoose.Schema(
-    {
-        time: {
-            type: String,
-            required: true
-        },
-
-        title: {
-            type: String,
-            required: true,
-            trim: true
-        },
-
-        description: {
-            type: String,
-            trim: true,
-            default: ""
-        },
-
-        completed: {
-            type: Boolean,
-            default: false
-        }
+  {
+    time: {
+      type: String,
+      required: true,
+      trim: true,
     },
-    {
-        timestamps: true
-    }
+
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Plan", planSchema);
