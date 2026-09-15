b6771c9463cf2784d8798b2b761e13636d99d7e5
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 13:16:49 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/App.jsx
+++ smart-productivity-dashboard/client/src/App.jsx
@@ -4,7 +4,7 @@ import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import Tasks from "./pages/Tasks";
 import Daily from "./pages/Daily";
-import Calendar from "./pages/Calendar";
+import Calendar from "./pages/calendar";
 import Settings from "./pages/settings";
 
 function App() {
