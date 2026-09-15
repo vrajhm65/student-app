ab00e3d2dab63b5fa4cd330c4891511b568491f5
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 18:46:17 +0530

    auto-commit

--- smart-productivity-dashboard/server/models/timer.js
+++ smart-productivity-dashboard/server/models/timer.js
@@ -1,20 +1,26 @@
 const mongoose = require("mongoose");
 
 const timerSchema = new mongoose.Schema(
-    {
-        seconds: {
-            type: Number,
-            default: 0
-        },
-
-        running: {
-            type: Boolean,
-            default: false
-        }
+  {
+    seconds: {
+      type: Number,
+      default: 0,
     },
-    {
-        timestamps: true
-    }
+
+    running: {
+      type: Boolean,
+      default: false,
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Timer", timerSchema);
