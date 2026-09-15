11491e435499553bd52caba5d60c9b3d0097ba31
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-15 13:25:33 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/App.jsx
+++ smart-productivity-dashboard/client/src/App.jsx
@@ -1,10 +1,11 @@
+```jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import Tasks from "./pages/Tasks";
 import Daily from "./pages/Daily";
-import Calendar from "./pages/calendar";
+import Calendar from "./pages/calender";
 import Settings from "./pages/settings";
 
 function App() {
@@ -25,3 +26,4 @@ function App() {
 }
 
 export default App;
+```
