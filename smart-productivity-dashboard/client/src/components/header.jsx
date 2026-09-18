0e6514c566831af2fbcf4bc7fa46afdff5a8f944
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-18 21:41:09 +0530

    auto-commit

--- smart-productivity-dashboard/client/src/components/header.jsx
+++ smart-productivity-dashboard/client/src/components/header.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import "./App.css";
 
 function getGreeting() {
   const hour = new Date().getHours();
