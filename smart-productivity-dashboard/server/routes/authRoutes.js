f6cf52470dece4f7b68a058474c83d067824a12a
Vinayraj hiremath <vrajhm65@gmail.com>
2026-09-21 18:47:35 +0530

    auto-commit

--- smart-productivity-dashboard/server/routes/authRoutes.js
+++ smart-productivity-dashboard/server/routes/authRoutes.js
@@ -2,12 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
+const protect = require("../middleware/authMiddleware");
+
 const {
     registerUser,
-    loginUser
+    loginUser,
+    getProfile,
+    updateProfile,
+    changePassword
 } = require("../controllers/authController");
 
+
+// Authentication
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+
+// Protected account routes
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
+router.put("/password", protect, changePassword);
+
 
 module.exports = router;
